73fbca9e4576442cfa28460a08db954ddc066aaf
recamanto <cedealejandro27@gmail.com>
2025-04-03 18:10:28 -0400

    juego_funcional_y_terminado

--- adivina_numero/adivina_el_numero.py
+++ adivina_numero/adivina_el_numero.py
@@ -1,1 +1,32 @@
-print("hola")
+import random
+
+print("--- ADIVINA EL NUMERO ENTRE EL 1 Y EL 10 --- TIENES 4 INTENTOS ---")
+numero_A = random.randint(1,10)
+usuario = int(input("INGRESE UN NUMERO: "))
+intentos = 1
+
+
+
+while (intentos != 4):
+    if(numero_A == usuario):
+        print("GANASTE. ADIVINASTE EL NUMERO")
+        break
+
+    elif(usuario > numero_A):
+        print("MUY ALTO")
+        intentos += 1
+        usuario = int(input("INGRESE UN NUMERO: "))
+        
+    elif(usuario < numero_A):
+        print("MUY BAJO")
+        intentos += 1
+        usuario = int(input("INGRESE UN NUMERO: "))
+else:
+    print("PERDISTE. INTENTOS AGOTADOS")
+
+        
+
+
+
+
+
